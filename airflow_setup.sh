0824b503e7358e0f058dbe5a43b4c0aa5d1780b3
ferruxtheviking <fernanbarba@hotmail.es>
2025-02-19 20:50:46 +0000

    Custom airflow image

--- airflow_setup.sh
+++ airflow_setup.sh
@@ -4,12 +4,10 @@
 echo "Launching docker-compose"
 docker compose up -d
 
-sleep 60
 # Create admin user
 echo "Creating Admin user"
 docker exec -d airflow airflow users create -e admin@example.org -f admin -l admin -r Admin -u admin -p admin
 
-sleep 3
 # Init the scheduler
 echo "Starting scheduler"
 docker exec -d airflow airflow scheduler
